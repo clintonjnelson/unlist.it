92af57e1fb14665823300526e6fe3878c70b8db4
clintonjnelson <clintonjnelson@live.com>
2014-08-17 00:42:50 -0700

    udpated category to return unposts in ASC order

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get     '/futurefeature',               to: 'pages#futurefeature'
   get     '/invalid_address',             to: 'pages#invalid_address'
   get     '/expired_link',                to: 'pages#expired_link'
-  get     '/invite-registration/:token',  to: 'users#new_with_invite', as: 'register_with_invite'
+  get     '/invite-registration/:token',  to: 'users#new_with_invite',     as: 'register_with_invite'
   get     '/invitation-sent',             to: 'pages#invitation_sent'
   get     '/safeguestsuccess',            to: 'pages#safeguestsuccess'
   get     '/signout',                     to: 'sessions#destroy'
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   get     '/userconfirmation/:token',     to: 'users#confirm_with_token',  as: 'userconfirmation'
   get     '/forgot_password',             to: 'forgot_passwords#new'
   post    '/forgot_password',             to: 'forgot_passwords#create'
-  get     '/safeguestconfirmation/:token',to: 'safeguests#create',          as: 'safeguestconfirmation'
+  get     '/safeguestconfirmation/:token',to: 'safeguests#create',         as: 'safeguestconfirmation'
   post    '/add_unimage',                 to: 'unimages#create'
   delete  '/remove_unimage',              to: 'unimages#destroy'
 
@@ -36,34 +36,34 @@ Rails.application.routes.draw do
   get     '/remove_radius',               to: 'searches#remove_radius'
   post    '/search_radius',               to: 'searches#set_search_radius'
 
-  resources :reset_passwords,       only: [:create, :show]
-  resources :sessions,              only: [:create]
-  #Protects Users by anonymity. BUILD THIS OUT - ADD SLUGS TOO
-  resources :unposts,               only: [:show, :index] do  #INDEX for general searching & use by non-creator
+  resources :reset_passwords,           only: [:create, :show]
+  resources :sessions,                  only: [:create]
+
+  resources :unposts,                   only: [:show, :index] do  #INDEX for general searching & use by non-creator
     collection do
-      post      :search,              to: 'searches#search'
+      post      :search,                  to: 'searches#search'
     end
-    resources :messages,            only: [:create, :index]
-    get       'show_message_form',    to: 'unposts#show_message_form' #AJAX
+    resources :messages,                only: [:create, :index]
+    get       'show_message_form',        to: 'unposts#show_message_form' #AJAX
   end
-  resources :users,                 only: [:create, :show, :edit, :update] do
-    resources :invitations,         only: [:new, :create]
-    resources :messages,            only: [:new, :create, :show, :index] #INDEX specific to user browsing own items
-    get       '/feedback',            to: 'messages#new_feedback'
-    resources :unposts,           except: [:index]
-    get       '/unlist',              to: 'unposts#index'
-    get       '/location_modal',      to: 'users#location_modal'
-    post      '/update_location',     to: 'users#update_location'
+  resources :users,                     only: [:create, :show, :edit, :update] do
+    resources :invitations,             only: [:new, :create]
+    resources :messages,                only: [:new, :create, :show, :index] #INDEX specific to user browsing own items
+    get       '/feedback',                to: 'messages#new_feedback'
+    resources :unposts,               except: [:index]
+    get       '/unlist',                  to: 'unposts#index'
+    get       '/location_modal',          to: 'users#location_modal'
+    post      '/update_location',         to: 'users#update_location'
   end
 
 
   namespace :admin do
     resources :categories
-    resources :conditions,             only: [:new, :create, :edit, :update, :destroy]
-    resources :settings,               only: [:edit, :update]
-    resources :users,                  only: [:index, :destroy]
-    post      '/conditions_by_category', to: 'conditions#conditions_by_category'
+    resources :conditions,              only: [:new, :create, :edit, :update, :destroy]
+    resources :settings,                only: [:edit, :update]
+    resources :users,                   only: [:index, :destroy]
+    post      '/conditions_by_category',  to: 'conditions#conditions_by_category'
   end
 
-  mount Sidekiq::Web,                 at: '/sidekiq'  #for online monitoring
+  mount Sidekiq::Web,                     at: '/sidekiq'  #for online monitoring
 end
