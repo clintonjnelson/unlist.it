00ca3784f04f18f111e43cb5139945b0d2b88cd2
clintonjnelson <clintonjnelson@live.com>
2014-08-19 20:18:43 -0700

    fixed migration syntax for Messages sender_id to change from string to integer. Required for PostgreSQL to accept in Heroku.

--- db/migrate/20140601002544_change_sender_id_type_in_messages.rb
+++ db/migrate/20140601002544_change_sender_id_type_in_messages.rb
@@ -1,5 +1,9 @@
 class ChangeSenderIdTypeInMessages < ActiveRecord::Migration
   def change
-    change_column :messages, :sender_id, :integer
+    #change_column :messages, :sender_id, :integer
+    # Change syntax for PostgreSQL to accept due to error:
+      # PG::DatatypeMismatch: ERROR:  column "sender_id" cannot be cast automatically to type integer
+      # HINT:  Specify a USING expression to perform the conversion.
+    change_column :messages, :sender_id, 'integer USING CAST(sender_id AS integer)'
   end
 end
