a5f3a794a816d5a39fdb404bb34e6136294ea3dd
clintonjnelson <clintonjnelson@live.com>
2014-08-23 15:12:03 -0700

    changed config.static_assets_compile to true to try to get images to show, changed favicon to filename for local assets

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compress = true
-  config.assets.compile  = false
+  config.assets.compile  = true #WAS FALSE
   config.assets.digest   = true
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version  = '1.0'
