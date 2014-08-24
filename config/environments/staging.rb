3c3a09e90d521f30b96eea6b8fc4014be57976da
clintonjnelson <clintonjnelson@live.com>
2014-08-23 22:15:37 -0700

    change consider_all_requests_local to true & setup sentry_raven

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,7 +3,7 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load    = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Assets Configs
--- config/initializers/sentry_raven.rb
+++ config/initializers/sentry_raven.rb
@@ -1,4 +1,9 @@
 # Include Sidekiq Errors in SentryRaven for Staging & Production
 if Rails.env.staging? || Rails.env.production?
   require 'raven/sidekiq'
+
+  Raven.configure do |config|
+    config.dsn = 'https://54f1a83f904a444baddbb9f2240ab9a1:d369e2a00fe3410ba4e59d605a499840@app.getsentry.com/29223'
+  end
 end
+
