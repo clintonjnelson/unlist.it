5615fa65083947186024a37d2533643c7f02c7ef
clintonjnelson <clintonjnelson@live.com>
2014-10-25 15:11:29 -0700

    updated config file to fix errors

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,35 +2,35 @@
 if Rails.env.production?
   SitemapGenerator::Sitemap.default_host = "http://www.unlist.it"
   SitemapGenerator::Sitemap.create_index = true
-  SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['PUBLIC_AWS_BUCKET']}.s3.amazonaws.com/"
+  SitemapGenerator::Sitemap.sitemaps_host = "http://s3.amazonaws.com/#{ENV['PUBLIC_AWS_BUCKET']}"
   SitemapGenerator::Sitemap.public_path = 'tmp/'
-  SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+  SitemapGenerator::Sitemap.sitemaps_path = ''
   SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
-  #SitemapGenerator::Sitemap.adapter.fog_directory = "#{ENV['PUBLIC_AWS_BUCKET']}"
+  SitemapGenerator::Sitemap.adapter.fog_directory = "#{ENV['PUBLIC_AWS_BUCKET']}"
 
-  SitemapGenerator::Sitemap.create do
-    # Put links creation logic here.
-    #
-    # The root path '/' and sitemap index file are added automatically for you.
-    # Links are added to the Sitemap in the order they are specified.
-    #
-    # Usage: add(path, options={})
-    #        (default options are used if you don't specify)
-    #
-    # Defaults: :priority => 0.5, :changefreq => 'weekly',
-    #           :lastmod => Time.now, :host => default_host
-    #
-    # Examples:
-    #
-    # Add '/articles'
-    #
-    #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-    #
-    # Add all articles:
-    # Article.find_each do |article|
-    #   add article_path(article), :lastmod => article.updated_at
-    # end
-  end
+  # SitemapGenerator::Sitemap.create do
+  #   # Put links creation logic here.
+  #   #
+  #   # The root path '/' and sitemap index file are added automatically for you.
+  #   # Links are added to the Sitemap in the order they are specified.
+  #   #
+  #   # Usage: add(path, options={})
+  #   #        (default options are used if you don't specify)
+  #   #
+  #   # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #   #           :lastmod => Time.now, :host => default_host
+  #   #
+  #   # Examples:
+  #   #
+  #   # Add '/articles'
+  #   #
+  #   #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #   #
+  #   # Add all articles:
+  #   # Article.find_each do |article|
+  #   #   add article_path(article), :lastmod => article.updated_at
+  #   # end
+  # end
 end
 
 
