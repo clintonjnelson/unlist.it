0184c845d22160f1ee28a48db88124802cc7d21e
clintonjnelson <clintonjnelson@live.com>
2014-11-22 17:59:19 -0800

    added js file for GA goal tag tracking, and added gem heroku-deflater

---
+++ app/assets/javascripts/ga-tracking.js
@@ -0,0 +1,25 @@
+/////THIS SHOULD BE A GENERIC JQUERY FUNCTION
+
+//Tracking the SignUp Button
+$('#signup-button').on('click', function() {
+  ga('send', 'event', 'button', 'click', 'signup');
+});
+
+
+//Tracking the Register Link
+$('#register-link').on('click', function() {
+  ga('send', 'event', 'link', 'click', 'register');
+});
+
+
+//Tracking Added Unlistings
+$('#addunlisting-button').on('click', function(){
+  ga('send', 'event', 'button', 'click', 'addunlisting');
+});
+
+
+//Tracking Added Friends
+$('#addfriend-link').on('click', function(){
+  ga('send', 'event', 'link', 'click', 'addfriend');
+});
+
