2e36216085e3d612868053969e2bc727f1918ff9
clintonjnelson <clintonjnelson@live.com>
2014-11-23 19:59:28 -0800

    updated click event tracking

--- app/assets/javascripts/ga-tracking.js
+++ app/assets/javascripts/ga-tracking.js
@@ -1,27 +1,23 @@
 /////THIS SHOULD BE A GENERIC JQUERY FUNCTION
 
-$(document).ready( function(){
+$(document).ready(function() {
   //Tracking the SignUp Button
   $('#signup-button').on('click', function() {
     ga('send', 'event', 'button', 'click', 'signup');
   });
-});
 
-$(document).ready( function(){
   //Tracking the Register Link
   $('#register-link').on('click', function() {
     ga('send', 'event', 'link', 'click', 'register');
   });
-});
 
-$(document).ready( function(){
+
   //Tracking Added Unlistings
   $('#addunlisting-button').on('click', function(){
     ga('send', 'event', 'button', 'click', 'addunlisting');
   });
-});
 
-$(document).ready( function(){
+
   //Tracking Added Friends
   $('#addfriend-link').on('click', function(){
     ga('send', 'event', 'link', 'click', 'addfriend');
