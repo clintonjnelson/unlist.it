e37034197e470e828507d659d6f6645c86519a2b
clintonjnelson <clintonjnelson@live.com>
2014-11-23 19:17:29 -0800

    event tracking tweaks to JS code

--- app/assets/javascripts/ga-tracking.js
+++ app/assets/javascripts/ga-tracking.js
@@ -1,25 +1,23 @@
 /////THIS SHOULD BE A GENERIC JQUERY FUNCTION
 
-//Tracking the SignUp Button
-$('#signup-button').on('click', function() {
-  ga('send', 'event', 'button', 'click', 'signup');
+$(document).ready( function(){
+  //Tracking the SignUp Button
+  $('#signup-button').on('click', function() {
+    ga('send', 'event', 'button', 'click', 'signup');
+  });
+
+  //Tracking the Register Link
+  $('#register-link').on('click', function() {
+    ga('send', 'event', 'link', 'click', 'register');
+  });
+
+  //Tracking Added Unlistings
+  $('#addunlisting-button').on('click', function(){
+    ga('send', 'event', 'button', 'click', 'addunlisting');
+  });
+
+  //Tracking Added Friends
+  $('#addfriend-link').on('click', function(){
+    ga('send', 'event', 'link', 'click', 'addfriend');
+  });
 });
-
-
-//Tracking the Register Link
-$('#register-link').on('click', function() {
-  ga('send', 'event', 'link', 'click', 'register');
-});
-
-
-//Tracking Added Unlistings
-$('#addunlisting-button').on('click', function(){
-  ga('send', 'event', 'button', 'click', 'addunlisting');
-});
-
-
-//Tracking Added Friends
-$('#addfriend-link').on('click', function(){
-  ga('send', 'event', 'link', 'click', 'addfriend');
-});
-
